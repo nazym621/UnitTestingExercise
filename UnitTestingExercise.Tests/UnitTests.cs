790fea2caedd2b218f1da1f538cdd50fe665265e
sanaz <sanazmarand@gmail.com>
2020-02-06 13:39:19 -0600

    adds

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,55 +1,67 @@
 using System;
 using Xunit;
+using UnitTestingExercise;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,1,1,3)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var addTest = new UnitTestMethods();
             //Act
+            var actual = addTest.Add(num1, num2, num3);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3,2,1)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var subTest = new UnitTestMethods();
 
             //Act
+            var actual = subTest.Subtract(minuend - subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,4,8)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var mulTest = new UnitTestMethods();
 
             //Act
+            var actual = mulTest.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,5,2)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var divTest = new UnitTestMethods();
 
             //Act
+            var actual = divTest.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
